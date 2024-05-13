d38226486f36b7f22dcf0b0f1ca0e81fbf70acfe
hurairaFurqan <hurairafurqan13@gmail.com>
2024-05-13 12:41:31 +1000

    build version

--- server/server.js
+++ server/server.js
@@ -12,12 +12,12 @@ const data = require('./routes/data_routes');
 // connect database
 connectDB();
 
-// cors
-const corsOptions = {
-    origin: /\.onrender\.com$/,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-};
-app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static("../client/build"));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+})
+app.use(cors());
 
 // initialize middleware
 app.use(express.json({ extended: false }));
@@ -25,6 +25,7 @@ app.get("/", (req, res) => res.send("Server is running"));
 
 // use routes
 app.use('/api/data', data);
+
 
 // setting up port
 const PORT = process.env.PORT || 8000;
